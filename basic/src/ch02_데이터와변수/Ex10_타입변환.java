71a80d6bed4fd2c81adf91789020aa6669505e23
fs-class <whyeil29@gmail.com>
2025-12-10 13:12:58 +0900

    데이터타입과변수

---
+++ basic/src/ch02_데이터와변수/Ex10_타입변환.java
@@ -0,0 +1,23 @@
+package ch02_데이터와변수;
+
+
+/**
+ * 타입 변환
+ * 1. 자동 타입 변환
+ * 		- 값의 허용 범위가 작은 타입이 허용 범위 큰 타입으로 대입될 때 발생한다.
+ * 		- char(2) -> int(4) -> long(8) -> float(4) -> double(8)
+ * 		- byte(1) -> short(2) -> int(4) -> long(8) -> float(4) -> double(8)
+ * 
+ * 2. 강제 타입 변환
+ * 		- 큰 허용 범위 타입을 작은 허용 범위 타입으로 쪼개어서 저장하는 것
+ * 		- 형식:
+ * 				작은타입 변수 = (작은타입) 변수
+ */
+public class Ex10_타입변환 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
